bc8fb5be8de6b043ad82a863e86f68646a9df4af
matthew <matthewjwu@gmail.com>
2019-05-22 00:16:37 -0700

    caching assets in sw

--- sw.js
+++ sw.js
@@ -1,9 +1,33 @@
+const staticCacheName = "site-static";
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/materialize.min.js',
+    '/css/styles.css',
+    '/css/materialize.min.css',
+    '/img/dish.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+]; // storing request URLs
+
 // install service worker
 self.addEventListener('install', event => {
-    console.log('service worker has been installed');
+    //console.log('service worker has been installed');
+    event.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    )
 });
 
 // activate event
 self.addEventListener('activate', event => {
-    console.log('service worker has been activated');
+    //console.log('service worker has been activated');
+});
+
+// fetch event
+self.addEventListener('fetch', event => {
+    //console.log('fetch event', event);
 });
