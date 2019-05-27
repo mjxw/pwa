11b21cb41e3b75f452500a227665848119927cf4
matthew <matthewjwu@gmail.com>
2019-05-26 17:36:18 -0700

    fallback page for cache miss

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const staticCacheName = "site-static";
+const staticCacheName = "site-static-v2"; //app shell cache
+const dynamicCacheName = 'site-dynamic-v1'
 const assets = [
     '/',
     '/index.html',
@@ -9,6 +10,8 @@ const assets = [
     '/css/materialize.min.css',
     '/img/dish.png',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+    '/pages/fallback.html '
 ]; // storing request URLs
 
 // install service worker
@@ -25,9 +28,29 @@ self.addEventListener('install', event => {
 // activate event
 self.addEventListener('activate', event => {
     //console.log('service worker has been activated');
+    event.waitUntil(
+        caches.keys().then(keys => { //access array of keys (cache versions)
+            //console.log(keys);
+            return Promise.all(keys //check for cache version and delete old caches
+            .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+            .map(key => caches.delete(key))
+            )
+        })
+    )
 });
 
 // fetch event
 self.addEventListener('fetch', event => {
     //console.log('fetch event', event);
+    event.respondWith(
+        caches.match(event.request).then((cacheRes) => {
+            //return pre-cached asset or return initial fetch request
+            return cacheRes || fetch(event.request).then((fetchRes) => {
+                return caches.open(dynamicCacheName).then((cache => {
+                    cache.put(event.request.url, fetchRes.clone()) //put clone of fetch response and put into cache as kv pair
+                    return fetchRes; // return the original fetch response so we consume it once.
+                }))
+            })
+        }).catch(() => caches.match('/pages/fallback.html'))
+    )
 });
